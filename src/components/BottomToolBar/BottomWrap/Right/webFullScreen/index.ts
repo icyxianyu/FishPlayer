ef3de74e32017945501d75d85c76d5754f3339fe
icyxianyu <3345625156@qq.com>
2023-08-07 19:41:42 +0800

    feat: 网页全屏

--- src/components/BottomToolBar/BottomWrap/Right/webFullScreen/index.ts
+++ src/components/BottomToolBar/BottomWrap/Right/webFullScreen/index.ts
@@ -3,15 +3,17 @@ import { Player } from "@/page";
 import { Video } from "@/player/video";
 import { Component } from "@/utils/createElement";
 import { createSVG } from "@/utils/createSVG";
-import { enterFull, exitFull, FullHTMLElement } from "@/utils/full";
+import { Store } from "@/store";
 
 class WebFullScreen extends Component {
     Player: Player;
     MiniScreenButton!: SVGSVGElement;
     FullScreenButton!: SVGSVGElement;
+    isFull: boolean = false;
     constructor(container: HTMLElement, video: Video, Player: Player) {
         super(container, "div", { class: "toolButton web-full-screen icon" });
         this.Player = Player;
+
         this.initIcon();
         this.initEvent();
     }
@@ -20,12 +22,37 @@ class WebFullScreen extends Component {
         this.FullScreenButton = createSVG(webFullScreen, '-2 -2 24 24');
         this.MiniScreenButton = createSVG(outFullScreen, '-2 -2 24 24');
         this.createText("网页全屏");
-        this.element.appendChild(this.FullScreenButton);    
+        this.element.appendChild(this.FullScreenButton);
+        this.element.appendChild(this.MiniScreenButton);
+        this.MiniScreenButton.style.display = "none";
     }
 
     initEvent() {
-
+        this.element.onclick = () => {
+            this.changeFull();
+            this.changeIcon();
+        }
     }
 
+    changeIcon() {
+        if (this.isFull) {
+            this.FullScreenButton.style.display = "none";
+            this.MiniScreenButton.style.display = "block";
+            this.changeText("退出网页全屏");
+        } else {
+            this.FullScreenButton.style.display = "block";
+            this.MiniScreenButton.style.display = "none";
+            this.changeText("网页全屏");
+        }
+    }
+
+    changeFull() {
+        if (this.isFull) {
+            this.Player.element.classList.remove("web-fullscreen");
+        } else {
+            this.Player.element.classList.add("web-fullscreen");
+        }
+        this.isFull = !this.isFull;
+    }
 }
 export default WebFullScreen;
--- src/utils/model.ts
+++ src/utils/model.ts
@@ -12,16 +12,7 @@ export class Modal extends Component {
         this.CurrentElement = new Component(this.element, type, props).element;
         this.time = 200;
         this.container = container;
-        this.initModalStyle();
         this.initModalEvent();
-    }
-
-    initModalStyle() {
-        // 在this.container位置的正上方出现
-        this.element.style.left = "50%";
-        this.element.style.transform = 'translate(-50%,-110%)';
-        this.element.style.backgroundColor = 'rgba(0,0,0,0.8)';
-        this.element.style.display = 'none';
     }
 
     initModalEvent() {
